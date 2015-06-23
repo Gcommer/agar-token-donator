704992c5a401b378d58d8112352138258664e6e9
Gcommer <gcommer@gmail.com>
2015-06-23 02:11:52 -0400

    python script: remove debugging statements

--- seeder.py
+++ seeder.py
@@ -12,18 +12,14 @@ AGARIO_HEADERS = {
 regions_modes = json.loads(requests.get('http://m.agar.io/info').text)["regions"]
 regions = filter(lambda r: ':' not in r, regions_modes.keys())
 
-print regions
-
 while True:
-    print "Try: ",
-
+#    print "Try: ",
     resp = requests.post('http://m.agar.io/',
                          headers=AGARIO_HEADERS,
                          data=random.choice(regions)).text
     server, token = resp.strip().split('\n')
 
-    print "connect(\"ws://{}\", \"{}\")".format(server, token)
-
+#    print "connect(\"ws://{}\", \"{}\")".format(server, token)
     requests.get(DONATE_URL.format(server, token))
 
     time.sleep(DELAY_SECS)
