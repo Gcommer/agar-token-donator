c65fdcc7300554a5de8883c37c976a241bfb353f
George Pittarelli <gjp@cs.umd.edu>
2015-06-23 12:04:59 -0400

    seeder script use firefox on windows UA

--- seeder.py
+++ seeder.py
@@ -3,10 +3,11 @@
 import requests, json, time, random
 
 DONATE_URL = 'http://at.gcommer.com/donate?server={}&token={}'
-DELAY_SECS = 20
+DELAY_SECS = 10
 AGARIO_HEADERS = {
     'Origin': 'http://agar.io',
-    'Referer': 'http://agar.io'
+    'Referer': 'http://agar.io',
+    'User-Agent': 'Mozilla/5.0 (Windows NT 6.3; rv:36.0) Gecko/20100101 Firefox/36.0'
 }
 
 regions_modes = json.loads(requests.get('http://m.agar.io/info').text)["regions"]
@@ -19,7 +20,8 @@ while True:
                          data=random.choice(regions)).text
     server, token = resp.strip().split('\n')
 
-#    print "connect(\"ws://{}\", \"{}\")".format(server, token)
+#    print "connect(\"ws://{}\", \"{}\")\r".format(server, token)
+
     requests.get(DONATE_URL.format(server, token))
 
     time.sleep(DELAY_SECS)
